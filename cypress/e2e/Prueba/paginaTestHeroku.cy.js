5e5d153525ba759f616f78c0c2842f05a4a7dbf8
STKPROJECTS\raquell.martinez <raquell.martinez@softtek.com>
2023-01-13 18:07:35 -0600

    se ajusta la prueba para demostración del funcionamiento de Page Object

--- cypress/e2e/Prueba/paginaTestHeroku.cy.js
+++ cypress/e2e/Prueba/paginaTestHeroku.cy.js
@@ -1,16 +1,14 @@
 import TestPageHeroku from "../../pages/herokuApp/TestPageHeroku";
 
 describe("Prueba Heroku App", () => {
-  const testPageHeroku = new TestPageHeroku(); //Sirve para los radios
+  const testPageHeroku = new TestPageHeroku();
 
-  it("Probar input", () => {
+  it("Probar formulario", () => {
     testPageHeroku.goToSite();
+    testPageHeroku.verifySite();
     testPageHeroku.setValueUsername("Hola");
     testPageHeroku.getValueUsername();
-    testPageHeroku.clearUsername();
     testPageHeroku.setValuePassword("secret");
-    testPageHeroku.getValuePassword();
-    testPageHeroku.clearPassword();
     testPageHeroku.clearComments();
     testPageHeroku.setValueComments("Hola soy un nuevo comentario");
     testPageHeroku.uploadFilename("ejemploParaSubir.txt");
